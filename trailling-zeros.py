364a56341157e3489bf29c8c7705a7b40c614fd0
anurag03 <anuragsinhab@gmail.com>
2016-02-13 02:01:29 +0530

    This is a program for solving trailling zero in factorial of a number problem.

---
+++ trailling-zeros.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+value = int(raw_input("Enter a value between 1 and 1000 :"))
+while value < 1 or value > 1000:
+	print "You have entered a invalid value"
+	value = int(raw_input("Enter the a value between 1 and 1000 :"))
+div = 5
+initial_value = 0
+while value > div:
+	initial_value = initial_value + (value / div)
+	div = div * 5
+print "The value of number of trailing-zeros are : " + str(initial_value)
