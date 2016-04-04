39750e04d379382a85a17d00a40b5ea49d8584f1
anurag03 <anuragsinhab@gmail.com>
2016-04-04 14:34:44 +0530

    Problem Solved

---
+++ roy-wobbly.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+test = int(raw_input())
+i = 0
+while test > i:
+	x = raw_input().split()
+	n = int(x[0])
+	k = int(x[1])
+	if 1 > test > 100 or 3 > n > 1000 or 1 > k > 99:
+		print "-1"
+	if k 
+	i = i + 1
